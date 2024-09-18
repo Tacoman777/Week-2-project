d8bc7a73941d89bee1184c59f302c1d934c240af
Tacoman777 <166954649+Tacoman777@users.noreply.github.com>
2024-09-18 12:57:34 -0400

    week 2

---
+++ main.js
@@ -0,0 +1,9 @@
+document.getElementById("h1").textContent = `Favorite Foods`;
+
+document.getElementById("l0").textContent = `Chicken`;
+document.getElementById("l2").textContent = `Mac and Cheese`;
+document.getElementById("l3").textContent = `Alfredo`;
+document.getElementById("l4").textContent = `Potato`;
+document.getElementById("l5").textContent = `Pizza`;
+
+console.log("I am liking JavaScript so far and I'm excited to see what else I can do in Javascript.");
